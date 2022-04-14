3615df1457d3f118cc55de18d85d0c8d068b121b
Elias Segundo <elias.segundo@luisrazo.local>
2022-04-14 11:10:02 -0500

    Creating job for credentials rotation

--- .test-infra/jenkins/job.groovy
+++ .test-infra/jenkins/job.groovy
@@ -25,7 +25,7 @@ job("Rotate clusters credentials") {
     // Set common parameters.
     // Sets that this is a cron job.
     steps {
-        shell("export PATH='/Users/elias.segundo/Documents/google-cloud-sdk/bin/gcloud:'$PATH")
+        shell("export PATH='/Users/elias.segundo/Documents/google-cloud-sdk/bin/:'$PATH")
         shell("echo $PATH")
         shell("printf 'yes'| gcloud container clusters update metrics-upgrade-clone --zone=us-central1-a --maintenance-window=06:00")
         shell("printf 'yes'| gcloud container clusters update cluster-io-datastores-clone --zone=us-central1-c --maintenance-window=06:00")
